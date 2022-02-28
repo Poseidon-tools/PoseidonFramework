4d6ab0be7f030a0c7ee45706280caf233a62ec0f
Muciojad <muciojad@gmail.com>
2022-02-28 12:06:40 +0100

    removed using

--- Runtime/Inseminator/Scripts/Resolver/Helpers/MethodsHelper.cs
+++ Runtime/Inseminator/Scripts/Resolver/Helpers/MethodsHelper.cs
@@ -3,7 +3,6 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
-    using Unity.VisualScripting;
     using UnityEngine;
 
     public static class MethodsHelper
